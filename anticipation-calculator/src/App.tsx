1795850eec77145032233e6686e26b6fc67b4981
unknown <guilhermekhirata@hotmail.com>
2023-01-16 18:01:38 -0300

    feat: criação e estilização do formulário

---
+++ anticipation-calculator/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { MainContent } from './components/MainContent';
+
+function App() {
+  return (
+    <>
+      <div id='body'>
+        <MainContent />
+      </div>
+    </>
+  );
+}
+
+export default App;
